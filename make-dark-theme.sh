2bec6865b11b898238c0539a5e5954fd9291ce22
Blair Drummond <blair.robert.drummond@gmail.com>
2019-03-19 12:43:45 -0400

    Make the color transformation more interesting

--- make-dark-theme.sh
+++ make-dark-theme.sh
@@ -1,4 +1,8 @@
-python negate.py leuven-theme.el > leuven-dark-theme.el
+python3 negate.py leuven-theme.el > leuven-dark-theme.el
+
+sed -i 's/leuven-theme\.el/leuven-dark-theme.el/g' leuven-dark-theme.el 
+sed -i 's/Awesome Emacs color theme on white background/Awesome Emacs color theme on dark background/' leuven-dark-theme.el 
+sed -i "s/(provide-theme 'leuven)/(provide-theme 'leuven-dark)/" leuven-dark-theme.el 
 
 echo "Colors inverted."
 echo "You probably still need to change the theme's name in"
